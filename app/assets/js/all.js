a398119ff5904553b59fb3dc00fe4a82aa6c88f2
Vic <Vic@gmail.com>
2020-05-12 17:06:58 +0800

    feat:admin__addAdmin

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,10 +1,9 @@
 
 
-// $(document).ready(function () {
-  
-//   $(".js-navbar__menu__link").click(function (e) {
-//     e.preventDefault();
-//     $(this).addClass("active");
-//     $(this).siblings().removeClass("active");
-//   });
-// });
+$(document).ready(() => {
+  // TODO:跳轉無效，待修正
+  $(".js-navbar__menu__link").click(function (e) {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
+});
