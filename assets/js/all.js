f054a4746af3bbf2e62688c80ce73f607a27e17a
syuan0212@gmail.com <hsuan777>
2020-05-15 12:28:34 +0800

    Update 2020-05-15T04:28:31.837Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,21 @@
 "use strict";
 
 $(document).ready(function () {
+  // 跳轉頁面時，判斷在哪一頁面並在 .navbar__menu後面的元素中 加上 .active
+  // 謝謝同學作業，以供參考
+  var title = $('title');
+
+  switch (title.text()) {
+    case "Assignment":
+      $('a[href="index.html"]').addClass('active');
+      break;
+
+    case "Admin":
+      $('a[href="admin.html"]').addClass('active');
+      break;
+  } // 箭頭方向改變，提醒有折疊內容
+
+
   $(".js-more").click(function () {
     if ($(this).text() != "expand_less") {
       $(this).text("expand_less");
@@ -30,7 +45,8 @@ $(document).ready(function () {
     $(this).find('.card-title').text(recipient);
     $(this).find('.js-serial_number').text(serialNumber);
     $(this).find('.js-user_email').text(userEmail);
-  }); // 解決切換 modal頁面時， modal無法往下拉，因為 data-dismiss會造成 modal-open被取消
+  }); // 點擊 userInfo 摺疊內容之 edit按鈕
+  // 解決切換 modal頁面時， modal無法往下拉，因為 data-dismiss會造成 modal-open被取消
 
   $('a[data-dismiss="modal"][data-toggle="modal"],button[data-dismiss="modal"][data-toggle="modal"]').on('click', function () {
     event.preventDefault();
